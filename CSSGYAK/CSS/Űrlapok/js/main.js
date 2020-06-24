a5d6a2914eadbc2bc778c41b92153c0d0ece28d6
Gferr56 <kovbe30@gmail.com>
2020-06-24 16:24:30 +0200

    init.page

--- CSSGYAK/CSS/Űrlapok/js/main.js
+++ CSSGYAK/CSS/Űrlapok/js/main.js
@@ -1,1 +1,14 @@
-console.log("This is Mel Brooks!");
+function hambiÁr() {
+    let ár = 1000;
+    let darab = document.querySelector("input[name='amount-input']");
+    let showTelj = document.querySelector("span.show-telj");
+    let teljes = parseInt(darab.value) * ár;
+    showTelj.innerHTML = teljes;
+    
+}
+
+
+
+
+
+
