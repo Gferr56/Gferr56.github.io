8300b7b7fc587b32a6df8dd91a1eefd420fe023e
Gferr56 <kovbe30@gmail.com>
2020-06-25 11:18:55 +0200

    init.page

--- CSSGYAK/CSS/Űrlapok/js/main.js
+++ CSSGYAK/CSS/Űrlapok/js/main.js
@@ -2,10 +2,20 @@ function hambiÁr() {
     let ár = 1000;
     let darab = document.querySelector("input[name='amount-input']");
     let showTelj = document.querySelector("span.show-telj");
-    let teljes = parseInt(darab.value) * ár;
-    showTelj.innerHTML = teljes;
-    
+    let valósDarab = parseInt(darab.value);
+    valósDarab = isNaN(valósDarab) ? 0 : valósDarab;
+    if ( valósDarab > 10 ) {
+        alert("Legfeljebb 10 darab rendelhető !")
+        }
+    else if( valósDarab < 1 ) {
+        alert("Legalább 1 darabot rendelj !")
+        }
+    else {
+        let teljes = parseInt(darab.value) * ár;
+        showTelj.innerHTML = teljes;
+        }
 }
+
 
 
 
