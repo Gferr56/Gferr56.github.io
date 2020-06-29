8897a0ba1a8cf4947b73a2d77dff74cd2a389b64
Gferr56 <kovbe30@gmail.com>
2020-06-29 14:47:55 +0200

    init.page

---
+++ CSSGYAK/CSS/Űrlapok/nevezetesalg.js
@@ -0,0 +1,46 @@
+/*
+ÓSSZEGZÉS ALGORITMUSA
+
+összeg = 0
+CIKLUS AMÍG van még szám, ADDIG
+  szám = következő elem
+  összeg = összeg + szám
+CIKLUS VÉGE
+
+
+SZÁMLÁLÁS ALORITMUSA
+
+
+db = 0
+CIKLUS AMÍG van még szám, ADDIG
+  szám = következő elem
+  HA igaz a feltétel szám-ra, AKKOR
+    db = db + 1
+  FELTÉTEL VÉGE
+CIKLUS VÉGE
+
+
+
+SZÉLSŐÉRTÉK ALGORITNUSA
+
+
+legnagyobb = első elem
+CIKLUS AMÍG van még szám, ADDIG
+  szám = következő elem
+  HA szám < legnagyobb, AKKOR
+    legnagyobb = szám
+  FELTÉTEL VÉGE
+CIKLUS VÉGE
+
+
+
+ELDÖNTÉS ALGORITMUSA
+
+találat = HAMIS
+CIKLUS AMÍG van elem ÉS találat = HANIS
+  szám = következő elem
+  HA igaz a feltétel szám-ra, AKKOR
+    találat = IGAZ
+  FELTÉTEL VÉGE
+CIKLUS VÉGE
+*/
