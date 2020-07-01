6475f106fcb3ea44397ab07e6c045e33510ed015
Gferr56 <kovbe30@gmail.com>
2020-07-01 17:12:51 +0200

    init.page

---
+++ CSSGYAK/CSS/Űrlapok/weather.js
@@ -0,0 +1,7 @@
+let temp = -1;
+function határ (ajánlat){
+    határ = (temp < 0 ? ajánlat = "...egy lélekmelegítő forró csoki?" : false )}
+     határ();
+     aktuális = ("Hő: " + temp + határ);
+     console.log(aktuális);
+     document.write(aktuális);
